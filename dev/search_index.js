20f5d335fbecd1edef6cc1c56663ab2df41ab242
Documenter.jl <documenter@juliadocs.github.io>
2023-05-10 14:30:18 +0000

    build based on 8e3981d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumMechanics","category":"page"},{"location":"#QuantumMechanics","page":"Home","title":"QuantumMechanics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumMechanics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumMechanics]","category":"page"}]
+}
