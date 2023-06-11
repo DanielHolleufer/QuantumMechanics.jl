e86f8c7c95d5d303efc3cea4758e648d72a1dd53
Daniel Holleufer <daniel.holleufer@gmail.com>
2023-06-11 19:10:34 +0200

    Updated Bra and Ket

--- src/states.jl
+++ src/states.jl
@@ -1,16 +1,61 @@
 abstract type QuantumState end
 abstract type StateVector <: QuantumState end
 
-mutable struct Ket <: StateVector
-    basis::Basis
-    data::Vector{ComplexF64}
+"""
+    Ket(b::Basis, data::AbstractArray{Number})
+
+Create a ket in the basis `b` with vector representation given by `data`.
+
+The vector representation is converted to a vector whose elements are complex floats.
+It also need not be normalized.
+
+# Examples
+```jldoctest
+julia> Ket(FockBasis(4), [1, 0, 0, 0, 0])
+Ket{FockBasis{Int64}, ComplexF64}(FockBasis{Int64}((5,), 4, 0), ComplexF64[1.0 + 0.0im, 0.0 + 0.0im, 0.0 + 0.0im, 0.0 + 0.0im, 0.0 + 0.0im])
+
+julia> Ket(SpinBasis(1 // 2), [1/sqrt(2), im/sqrt(2)])
+Ket{SpinBasis{1//2, Int64}, ComplexF64}(SpinBasis{1//2, Int64}((2,), 1//2), ComplexF64[0.7071067811865475 + 0.0im, 0.0 + 0.7071067811865475im])
+```
+"""
+mutable struct Ket{B,T} <: StateVector
+    basis::B
+    data::Vector{T}
+    function Ket(b::B, data::AbstractArray{T}) where {B<:Basis,T<:Number}
+        if length(b) !== length(data)
+            error("Dimension of data does not match dimension of basis.")
+        end
+        return new{B,complex(float(T))}(b, vec(data))
+    end
 end
 Base.:(==)(u::Ket, v::Ket) = u.data == v.data && u.basis == v.basis
 
+"""
+    Bra(b::Basis, data::AbstractArray{Number})
 
-mutable struct Bra <: StateVector
+Create a bra in the basis `b` with vector representation given by `data`.
+
+The vector representation is converted to a vector whose elements are complex floats.
+It also need not be normalized.
+
+# Examples
+```jldoctest
+julia> Bra(FockBasis(4), [1, 0, 0, 0, 0])
+Bra{FockBasis{Int64}, ComplexF64}(FockBasis{Int64}((5,), 4, 0), ComplexF64[1.0 + 0.0im, 0.0 + 0.0im, 0.0 + 0.0im, 0.0 + 0.0im, 0.0 + 0.0im])
+
+julia> Bra(SpinBasis(1 // 2), [1/sqrt(2), im/sqrt(2)])
+Bra{SpinBasis{1//2, Int64}, ComplexF64}(SpinBasis{1//2, Int64}((2,), 1//2), ComplexF64[0.7071067811865475 + 0.0im, 0.0 + 0.7071067811865475im])
+```
+"""
+mutable struct Bra{B,T} <: StateVector
     basis::Basis
-    data::Vector{ComplexF64}
+    data::Vector{T}
+    function Bra(b::B, data::AbstractArray{T}) where {B<:Basis,T<:Number}
+        if length(b) !== length(data)
+            error("Dimension of data does not match dimension of basis.")
+        end
+        return new{B,complex(float(T))}(b, vec(data))
+    end
 end
 Base.:(==)(u::Bra, v::Bra) = u.data == v.data && u.basis == v.basis
 
--- test/states.jl
+++ test/states.jl
@@ -1,27 +1,38 @@
 using QuantumMechanics
 using Test
 
-@testset "StateVectors" begin
-    v = [1.0 / sqrt(2.0) + 0.0im, 0.0 - 1.0im / 2.0, 0.0 + 0.0im, -1.0 / 2.0 + 0.0im]
-    b = GenericBasis(4)
-    ψ = Ket(b, v)
-    @test ψ == ψ
+@testset verbose = true "StateVectors" begin
+    @testset "Bra and Ket" begin
+        u_1 = [0.0 + 0.0im, 0.0 - 0.0im, 0.0 + 1.0im / 2, 0.0 + 0.0im]
+        v_1 = [1.0 / sqrt(2.0) + 0.0im, 0.0 - 1.0im / 2.0, 0.0 + 0.0im, -1.0 / 2.0 + 0.0im]
+        basis_1 = GenericBasis(4)
+        ϕ_1 = Bra(basis_1, u_1)
+        ψ_1 = Ket(basis_1, v_1)
+        @test ψ_1 == ψ_1
+        @test ϕ_1 == ϕ_1
+        @test ψ_1 != ϕ_1
+        @test ϕ_1 * ψ_1 ≈ 0.0 atol = 1.0e-12
 
-    ϕ = Bra(b, v)
-    @test ϕ == ϕ
-    @test ψ != ϕ
+        u_2 = [1 / sqrt(2) -1 / sqrt(2)]
+        v_2 = [1 0]
+        w_2 = [1.0 / sqrt(2), 1.0im / sqrt(2)]
+        basis_2 = GenericBasis(2)
+        ϕ_2 = Bra(basis_2, u_2)
+        ψ_2 = Ket(basis_2, v_2)
+        @test ψ_2 == ψ_2
+        @test ϕ_2 == ϕ_2
+        @test ψ_2 != ϕ_2
+        @test ϕ_2 * ψ_2 ≈ 1 / sqrt(2) atol = 1.0e-12
+        @test typeof(Bra(basis_2, u_2)) == typeof(Bra(basis_2, v_2))
+        @test typeof(Bra(basis_2, u_2)) == typeof(Bra(basis_2, w_2))
+        @test typeof(Ket(basis_2, u_2)) == typeof(Ket(basis_2, v_2))
+        @test typeof(Ket(basis_2, u_2)) == typeof(Ket(basis_2, w_2))
+        @test typeof(Bra(basis_2, u_2)) != typeof(Ket(basis_2, w_2))
 
-    @test ϕ * ψ ≈ 1.0 atol = 1.0e-12
-
-
-    u = [0.0 + 1.0im / sqrt(3.0) 0.0 - 0.0im exp(-2.0im) * sqrt(2 / 3)]
-    b = GenericBasis(3)
-    ψu = Ket(b, v)
-    @test ψu == ψu
-
-    ϕu = Bra(b, v)
-    @test ϕu == ϕu
-    @test ψu != ϕu
-
-    @test ϕu * ψu ≈ 1.0 atol = 1.0e-12
+        @test_throws Exception Bra(basis_1, u_2)
+        @test_throws Exception Bra(basis_2, u_1)
+        @test_throws Exception Bra(
+            basis_1, (0.0 + 0.0im, 0.0 - 0.0im, 0.0 + 1.0im / 2, 0.0 + 0.0im)
+        )
+    end
 end
